28e3e1b545574c49642e26c1481e60e44b109100
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-03 22:23:08 +0000

    search ouput updated

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import { debounceTime, filter, fromEvent, map, of, switchMap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import {
+  catchError,
+  debounceTime,
+  delay,
+  filter,
+  finalize,
+  fromEvent,
+  map,
+  Observable,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss',
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   items: string[] = [];
-  searchItem: string = '';
-
+  loading: boolean = false;
+  error: string | null = null;
+  notFound: string = '';
 
   // Mock data array
   private mockData: string[] = [
@@ -34,18 +47,40 @@ export class SearchComponent {
 
     fromEvent(searchInput, 'input')
       .pipe(
-        debounceTime(300),
         map((event: any) => event.target.value),
+        debounceTime(300),
         filter((term: string) => term.length >= 3),
-        switchMap((searchTerm: string) => this.searchItems(searchTerm))
+        tap(() => {
+          this.loading = true;
+          this.error = null;
+          this.items = [];
+          this.notFound = ''; // Reset not found message
+        }),
+        switchMap((searchTerm: string) =>
+          this.searchItems(searchTerm).pipe(
+            // Call searchItems directly
+            delay(400),
+            catchError((err) => {
+              this.error = 'Failed to search items.';
+              this.loading = false;
+              return of([]); // Return an empty array on error
+            }),
+            tap(() => (this.loading = false))
+          )
+        )
       )
-      .subscribe((results: string[]) => {
+      .subscribe((results) => {
         this.items = results;
+        if (this.items.length === 0) {
+          this.notFound = 'No results found.';
+        } else {
+          this.notFound = '';
+        }
       });
   }
 
   // Search function to filter mock data
-  searchItems(searchTerm: string) {
+  searchItems(searchTerm: string): Observable<string[]> {
     const filteredResults = this.mockData.filter((item) =>
       item.toLowerCase().includes(searchTerm.toLowerCase())
     );
